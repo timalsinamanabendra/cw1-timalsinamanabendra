a2bacd51c561187553b112830560bccb39e744d9
timalsinamanabendra <97384274+timalsinamanabendra@users.noreply.github.com>
2023-07-24 02:19:06 +0545

    Create customer_home.php

---
+++ customer_home.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Ecommerce</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <!-- bootstrap links -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- bootstrap links -->
+    <!-- fonts links -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+    <!-- fonts links -->
+</head>
+<body>
+
+    <!-- top navbar -->
+    <div class="top-navbar">
+        <div class="top-icons">
+            <a href="https://www.facebook.com/prabesh.timalsina.58" target="_blank" class="facebook"><i  class="fa-brands fa-facebook-f"></i></a>
+            <a href="https://www.instagram.com/prash9356/" target="_blank" class="instagram"> <i class="fa-brands fa-instagram"></i></a>
+            <a href="https://www.linkedin.com/in/manabendra-timalsina-ab147a277/" target="_blank" class="linkin"><i class="fa-brands fa-linkedin-in"></i></a>
+        </div>
+        <div class="other-links">
+            <a href="login.php"><button id="btn-login">Login</button></a>
+            <a href="signup.php" ><button id="btn-signup">Sign up</button></a>
+
+                <i class="fa-solid fa-user">
+                        
+                </i>
+            <i class="fa-solid fa-cart-shopping"></i>
+        </div>
+    </div>
+    <!-- top navbar -->
+
+    <div class="home-section">
+        <!-- navbar -->
+        <nav class="navbar navbar-expand-lg" id="navbar">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="customer_home.php" style="color: black;">EE</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span><i class="fa-solid fa-bars" style="color: white;"></i></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="customer_home.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="customer_product.php">Product</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="customer_about.php">About Us</a>
+                  </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="customer_contact.php">Contact Us</a>
+                  </li>
+                </ul>
+               
+                <form class="d-flex">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit" id="search-btn">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+        <!-- navbar -->
+
+        <!-- home content -->
+        <section class="home">
+            <div class="content">
+                <h3>Biggest Clothe Sale
+                    <br> <span>Up To 50% Off</span>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque culpa, totam sed maxime animi facilis!</p>
+                <button id="shopnow">Shop Now</button>
+            </div>
+            <div class="img">
+                <img src="./image/b2.png" alt="">
+            </div>
+        </section>
+        <!-- home content -->
+    </div>
+
+    <!-- top cards -->
+    <div class="container" id="top-cards">
+        <div class="row">
+            <div class="col-md-5 py-3 py-md-0">
+                <div class="card" style="background-color: #a9a9a926;">
+                    <img src="./image/topcard1.png" alt="">
+                    <div class="card-img-overlay">
+                        <h5 class="card-titel">Smart Watch</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, ratione!</p>
+                        <p><strong>$200.30 <strike>$250.10</strike></strong></p>
+                        <button>Order Now</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 py-3 py-md-0">
+                <div class="card" style="background-color: #a9a9a926;">
+                    <img src="./image/topcard2.png" alt="">
+                    <div class="card-img-overlay">
+                        <h5 class="card-titel">Nike Shoes</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, ratione!</p>
+                        <p><strong>$150.10 <strike>$200.10</strike></strong></p>
+                        <button>Order Now</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 py-3 py-md-0">
+                <div class="card" style="background-color: #a9a9a926;">
+                    <img src="./image/topcard3.png" alt="">
+                    <div class="card-img-overlay">
+                        <h5 class="card-titel">Bag</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                        <p><strong>$50.10 <strike>$60</strike></strong></p>
+                        <button>Order Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- top cards -->
+    
+
+    <!-- banner -->
+    <div class="banner">
+        <div class="content">
+            <h1>Get Up To 50% Off</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, animi?</p>
+            <div id="bannerbtn"><button>SHOP NOW</button></div>
+        </div>
+    </div>
+    <!-- banner -->
+
+    <!-- product cards -->
+    <div class="container" id="product-cards">
+        <h1 class="text-center">PRODUCT</h1>
+        <div class="row" style="margin-top: 30px;">
+            <div class="col-md-3 py-3 py-md-0">
+                <div class="card">
+                    <img src="./image/p13.png" alt="">
+                    <div class="card-body">
+                        <h3>Men T-Shirt</h3>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <div class="star">
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                        </div>
+                        <h5>$32.60 <strike>$50</strike> <span><i class="fa-solid fa-cart-shopping"></i></span></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 py-3 py-md-0">
+                <div class="card">
+                    <img src="./image/p14.png" alt="">
+                    <div class="card-body">
+                        <h3>Purple Heel</h3>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <div class="star">
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                        </div>
+                        <h5>$56.50 <strike>$60</strike> <span><i class="fa-solid fa-cart-shopping"></i></span></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 py-3 py-md-0">
+                <div class="card">
+                    <img src="./image/p15.png" alt="">
+                    <div class="card-body">
+                        <h3>Men Shoes</h3>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <div class="star">
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                        </div>
+                        <h5>$150.5 <strike>$160.50</strike> <span><i class="fa-solid fa-cart-shopping"></i></span></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 py-3 py-md-0">
+                <div class="card">
+                    <img src="./image/p16.png" alt="">
+                    <div class="card-body">
+                        <h3>Men Jacket</h3>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <div class="star">
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                        </div>
+                        <h5>$50.60 <strike>$56.90</strike> <span><i class="fa-solid fa-cart-shopping"></i></span></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row" style="margin-top: 30px;">
+            <div class="col-md-3 py-3 py-md-0">
+                <div class="card">
+                    <img src="./image/p17.png" alt="">
+                    <div class="card-body">
+                        <h3>Blue T-Shirt</h3>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <div class="star">
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                        </div>
+                        <h5>$5.60 <strike>$10.50</strike> <span><i class="fa-solid fa-cart-shopping"></i></span></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 py-3 py-md-0">
+                <div class="card">
+                    <img src="./image/p18.png" alt="">
+                    <div class="card-body">
+                        <h3>Girls Sandal</h3>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <div class="star">
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                        </div>
+                        <h5>$2.30 <strike>$5</strike> <span><i class="fa-solid fa-cart-shopping"></i></span></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 py-3 py-md-0">
+                <div class="card">
+                    <img src="./image/p19.png" alt="">
+                    <div class="card-body">
+                        <h3>Men Jacket</h3>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <div class="star">
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                        </div>
+                        <h5>$3.2 <strike>$5.60</strike> <span><i class="fa-solid fa-cart-shopping"></i></span></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 py-3 py-md-0">
+                <div class="card">
+                    <img src="./image/p20.png" alt="">
+                    <div class="card-body">
+                        <h3>Men T-SHirt</h3>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <div class="star">
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                            <i class="fas fa-star checked"></i>
+                        </div>
+                        <h5>$10.50 <strike>$15.60</strike> <span><i class="fa-solid fa-cart-shopping"></i></span></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a href="clothe.html" id="viewmorebtn">View More</a>
+    </div>
+    <!-- product cards -->
+
+    <!-- product -->
+    <div class="container" style="margin-top: 100px;">
+    <hr>
+</div>
+<div class="container">
+    <h3 style="font-weight: bold;">PRODUCT.</h3>
+    <p style="text-align:justify;">Products are tangible or intangible items that are created, manufactured, or provided for sale, consumption, or use. They come in various forms and serve different purposes, ranging from physical goods like electronics and household appliances to digital products such as software, e-books, and online courses.<br>
+
+Physical products are the most common type and include a wide range of items we use in our daily lives. These can be categorized into various sectors, including consumer goods, industrial goods, and capital goods. Consumer goods are intended for personal use, such as clothing, food, and personal care items. Industrial goods are used as inputs in the production of other goods, such as raw materials, machinery, and equipment. Capital goods are long-lasting goods used to produce other goods and services, like buildings and infrastructure.<br>
+
+In addition to physical products, there are also digital products that have gained prominence with the advancement of technology. These include software applications, digital media, online services, and virtual goods. Digital products offer convenience, instant access, and the ability to be easily distributed and updated.<br>
+
+The development and marketing of products involve various stages, including research and development, design, production, and distribution. Companies strive to create products that meet customer needs, offer unique features or benefits, and provide value for money. The success of a product often relies on effective branding, positioning, and marketing strategies to attract and retain customers.<br>
+
+Product quality is crucial in establishing customer satisfaction and loyalty. Customers expect products to be reliable, durable, and perform their intended functions effectively. As a result, companies invest in quality control measures, testing, and certifications to ensure their products meet industry standards and customer expectations.<br>
+
+With the rise of e-commerce, products are now more accessible than ever. Online platforms provide a global marketplace where businesses can showcase and sell their products to a vast customer base. Customers can browse through a wide range of products, compare prices, read reviews, and make purchases from the comfort of their homes.<br>
+
+In summary, products encompass a broad spectrum of tangible and intangible items that are created and offered for sale or consumption. They vary from physical goods to digital products and play a significant role in our daily lives. The development, quality, and marketing of products are essential factors in meeting customer needs and ensuring business success.</p>
+
+    <hr>
+</div>
+    <!-- product -->
+
+
+
+
+
+
+<!-- footer -->
+<footer id="footer" style="margin-top: 50px;">
+<div class="footer-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 footer-content">
+                <h3>E-Electronic</h3>
+                <p>E-Electronic, or electronic commerce (e-commerce), refers to the buying and selling of goods and services over the internet.</p>
+                <p>
+                    Bhaisepati <br>
+                    Lalitpur<br>
+                    Nepal<br>
+                </p>
+                <strong><i class="fas fa-phone"></i> Phone: <strong>+97798********</strong></strong><br>
+                <strong><i class="fa-solid fa-envelope"></i> Email: <strong>prabesh.timalsina1234@gmail.com</strong></strong>
+            </div>
+            <div class="col-lg-3 col-md-6 footer-links">
+                <ul>
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="about.html">About</a></li>
+                  <li><a href="contact.html">Contact</a></li>
+                  <li><a href="#">Services</a></li>
+                  <li><a href="#">Privacy policay</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 footer-links">
+                <h4>Our Services</h4>
+                <p>We are here to delivert products in time!!...</p>
+                <ul>
+                    <li><a href="#">Luxirious Products</a></li>
+                    <li><a href="#">Home Appliance</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 footer-links">
+                <h4>Our Social World</h4>
+                <p>Our Social Worlds is for Providing services and to our valuable customer......</p>
+                <div class="socail-links mt-3">
+                    <a href="https://www.facebook.com/prabesh.timalsina.58" target="_blank" class="facebook"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="https://www.instagram.com/prash9356/" target="_blank" class="instagram"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/manabendra-timalsina-ab147a277/" target="_blank" class="linkin"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+<div class="container py-4">
+    <div class="copyright">
+        &copy; Copyright <strong>E-Electronics</strong>.All Rights Reserved
+    </div>
+    <div class="credits">
+        Designed By <a href="#">Manabendra Timalsina</a>
+    </div>
+</div>
+</footer>
+<!-- footer -->
+
+<a href="#" class="arrow"><i><img src="./image/up-arrow.png" alt="" width="50px"></i></a>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
