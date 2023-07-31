cca25aeb78b08b313556e7b0e3e3183abd109ef4
timalsinamanabendra <97384274+timalsinamanabendra@users.noreply.github.com>
2023-08-01 01:25:48 +0545

    Update login.php

--- login.php
+++ login.php
@@ -1,182 +1,86 @@
 <?php
-
+// config.php
 @include 'config.php';
-
-if(isset($_POST['submit'])){
-
-
-$email = mysqli_real_escape_string($conn, $_POST['email']);
-$password = md5($_POST['password']);
-
-
-   $select = " SELECT * FROM signup WHERE email = '$email' && password = '$password' ";
-
-   $result = mysqli_query($conn, $select);
-
-   if(mysqli_num_rows($result) > 0){
-
-      $row = mysqli_fetch_array($result);
-
-      if($row['usertype'] == 'admin'){
-
-         $_SESSION['admin_name'] = $row['firstname'];
-         header('location:admin_home.php');
-
-      }elseif($row['usertype'] == 'customer'){
-
-         $_SESSION['customer_name'] = $row['firstname'];
-         header('location:customer_home.php');
-
-      }
-      elseif($row['usertype'] == 'seller'){
-
-         $_SESSION['seller_name'] = $row['firstname'];
-         header('location:seller_home.php');
-
-      }
-     
-   }else{
-      $error[] = 'incorrect email or password!';
-   }
-
+if(!isset($_SESSION['admin_name'])){
+   header('location:login.php');
 };
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-Ecommerce</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    <!-- bootstrap links -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <!-- bootstrap links -->
-    <!-- fonts links -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
-    <!-- fonts links -->
-</head>
-<body>
-    <!-- top navbar -->
-    <div class="top-navbar">
-        <div class="top-icons">
-            <a href="https://www.facebook.com/prabesh.timalsina.58" target="_blank" class="facebook"><i  class="fa-brands fa-facebook-f"></i></a>
-            <a href="https://www.instagram.com/prash9356/" target="_blank" class="instagram"> <i class="fa-brands fa-instagram"></i></a>
-            <a href="https://www.linkedin.com/in/manabendra-timalsina-ab147a277/" target="_blank" class="linkin"><i class="fa-brands fa-linkedin-in"></i></a>
-        </div>
-        <div class="other-links">
-        </div>
-    </div>
-    <!-- top navbar -->
 
-    <div class="home-section">
-        <!-- navbar -->
-        <nav class="navbar navbar-expand-lg" id="navbar">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="home.php" style="color: black;">EE</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span><i class="fa-solid fa-bars" style="color: white;"></i></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="home.php">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="product.php">Product</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="about.php">About Us</a>
-                  </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contact.php">Contact Us</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        <!-- navbar -->
+if(isset($_POST['update'])){
+     header('Location: userdata.php');
 
-            <!-- login -->
-            <form action="" method="post">
-        <div class="container login">
-            <div class="row">
-                <div class="col-md-4" id="side1">
-                    <h3>Hi!!!!! If u don't have account account then click on signup</h3>
-                    <div id="btn"><a href="signup.php">Sign up</a></div>
-                </div>
-                <div class="col-md-8" id="side2">
-                    <h3>Login</h3>
-                    <div class="inp">
-                              <input type="email" name="email" required placeholder="Enter Your Email">
-                              <input type="password" name="password" required placeholder="Enter Your password">
-                    </div>
-<div id="login"><button type="submit" class="btn btn-danger" value="login now" name="submit">Login</button>  </div>              </div>
-            </div>
-        </div></form>
-        <!-- login -->
-
-        <!-- footer -->
-<footer id="footer" style="margin-top: 50px;">
-<div class="footer-top">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3 col-md-6 footer-content">
-                <h3>E-Electronic</h3>
-                <p>E-Electronic, or electronic commerce (e-commerce), refers to the buying and selling of goods and services over the internet.</p>
-                <p>
-                    Bhaisepati <br>
-                    Lalitpur<br>
-                    Nepal<br>
-                </p>
-                <strong><i class="fas fa-phone"></i> Phone: <strong>+97798********</strong></strong><br>
-                <strong><i class="fa-solid fa-envelope"></i> Email: <strong>prabesh.timalsina1234@gmail.com</strong></strong>
-            </div>
-            <div class="col-lg-3 col-md-6 footer-links">
-                <ul>
-                  <li><a href="home.php">Home</a></li>
-                  <li><a href="about.php">About</a></li>
-                  <li><a href="contact.php">Contact</a></li>
-                </ul>
-            </div>
-            <div class="col-lg-3 col-md-6 footer-links">
-                <h4>Our Services</h4>
-                <p>We are here to delivert products in time!!...</p>
-            </div>
-            <div class="col-lg-3 col-md-6 footer-links">
-                <h4>Our Social World</h4>
-                <p>Our Social Worlds is for Providing services and to our valuable customer......</p>
-                <div class="socail-links mt-3">
-                    <a href="https://www.facebook.com/prabesh.timalsina.58" target="_blank" class="facebook"><i class="fa-brands fa-facebook-f"></i></a>
-                    <a href="https://www.instagram.com/prash9356/" target="_blank" class="instagram"><i class="fa-brands fa-instagram"></i></a>
-                    <a href="https://www.linkedin.com/in/manabendra-timalsina-ab147a277/" target="_blank" class="linkin"><i class="fa-brands fa-linkedin-in"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<hr>
-<div class="container py-4">
-    <div class="copyright">
-        &copy; Copyright <strong>E-Electronics</strong>.All Rights Reserved
-    </div>
-    <div class="credits">
-        Designed By <a href="#">Manabendra Timalsina</a>
-    </div>
-</div>
-</footer>
-<!-- footer -->
-<script>
-    if ( window.history.replaceState ) {
-        window.history.replaceState( null, null, window.location.href );
+}
+// Function to get user data from the database based on user_id
+function getUserData($conn, $user_id) {
+    $query = "SELECT * FROM `signup` WHERE user_id = '$user_id'";
+    $result = mysqli_query($conn, $query);
+    if ($result && mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        return $row;
     }
-</script>
+    return false;
+}
 
-<script src="script.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+if (isset($_POST['update'])) {
+    $user_id = $_GET['user_id'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $pnumber = $_POST['pnumber'];
 
-</body>
+    $query = "UPDATE `signup` SET firstname='$firstname', lastname='$lastname', email='$email', pnumber='$pnumber' WHERE user_id='$user_id'";
+    $query_run = mysqli_query($conn, $query);
+
+    if ($query_run) {
+        echo "<script type='text/javascript'>alert('Data updated');</script>";
+    } else {
+        echo "<script type='text/javascript'>alert('Data Not updated');</script>";
+    }
+}
+
+?>
+
+<!-- edit page of user data -->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" />
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+  <body>
+    <div class="updateformdata"> 
+      <div class="center">
+          <h1>Update Data</h1>
+          <?php
+          // Fetch the user data based on the provided user_id
+          if (isset($_GET['user_id'])) {
+              $user_id = $_GET['user_id'];
+              $userData = getUserData($conn, $user_id);
+          }
+          ?>
+          <form method="POST" action="userupdatedata.php?user_id=<?php echo $user_id; ?>">
+                <div class="txt_field">
+                    <input type="text" name="firstname" required value="<?php echo isset($userData['firstname']) ? $userData['firstname'] : ''; ?>">
+                    <label>First Name</label>
+                </div>
+                <div class="txt_field">
+                    <input type="text" name="lastname" required value="<?php echo isset($userData['lastname']) ? $userData['lastname'] : ''; ?>">
+                    <label>Last Name</label>
+                </div>
+                <div class="txt_field">
+                    <input type="email" name="email" required value="<?php echo isset($userData['email']) ? $userData['email'] : ''; ?>">
+                    <label>Email</label>
+                </div>
+                <div class="txt_field">
+                    <input type="tel" name="pnumber" required value="<?php echo isset($userData['pnumber']) ? $userData['pnumber'] : ''; ?>">
+                    <label>Phone Number</label>
+                </div>
+
+            <button type="submit" name="update" value="Update">Update</button>
+          </form>
+      </div>
+  </div>
+
+  </body>
 </html>
